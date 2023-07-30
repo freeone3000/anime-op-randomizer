551f35e9b60721348a212d62166a5b0c6fef8ef7
Jasmine Moore <jamoo@jamoo.dev>
2023-07-29 21:41:09 -0400

    world's worst HTTP server

--- train/test.py
+++ train/test.py
@@ -12,16 +12,23 @@ import video
 TCP_PORT = 8091
 BUF_SIZE = 4096  # also the packet size for UDP
 
+
+def handle_http_headers(conn: socket.socket):
+    conn.send("HTTP/1.1 200 OK\r\n".encode("utf-8"))
+    conn.send("Content-Type: video/mp4\r\n".encode("utf-8"))
+    conn.send("Transfer-Encoding: chunked\r\n".encode("utf-8"))
+    conn.send("\r\n".encode("utf-8"))
+
+
 def serve(directory: str):
     (playlist, _) = scan(directory)
     print(playlist)
 
-    # TODO handle HTTP
-    # TODO Make all videos same resolution (1080p)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss:
         ss.bind(("0.0.0.0", TCP_PORT))
         ss.listen()
         (conn, addr) = ss.accept()
+        handle_http_headers(conn)
 
         while True:
             random.shuffle(playlist)
@@ -32,7 +39,9 @@ def serve(directory: str):
                         data = stream.read(BUF_SIZE)
                         if not data:
                             break
+                        conn.send("%x\r\n".encode("utf-8") % len(data)) # size of chunk
                         conn.sendall(data)
+                        conn.send("\r\n".encode("utf-8")) # eoc
 
 
 if __name__ == "__main__":
